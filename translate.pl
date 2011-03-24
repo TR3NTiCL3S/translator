4a9fd42d60a03bdcb43ba51ce9227943b36320c4
Z. Cliffe Schreuders <z.cliffe@schreuders.org>
2011-03-25 01:43:59 +0800

    inital commit

---
+++ translate.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+# Simple slang translater example
+# Copyleft (ↄ) 2010-2011
+# Z. Cliffe Schreuders
+
+# This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+my %translations;
+my $filename = "slang.txt";
+my $fileline;
+
+# read in translations
+open(IN, "$filename") or die "Can't open $filename: $!";
+while(chomp($fileline = <IN>)) {
+	# read translation lines
+	if($fileline =~ /(.+):(.+)/) {
+		$translations{$1} = $2;
+	# ignore blank lines or comments
+	} elsif($fileline =~ /^\s*#|^\s*$/) {
+	} else {
+		warn "$filename includes an invalid line: $fileline\n";
+	}
+}
+
+# read line to translate
+print "Welcome to the example slang translater program\n";
+print "Please enter a sentence to translate\n";
+chomp($line = <STDIN>);
+
+# do conversions
+foreach $key (keys %translations) {
+	$line =~ s/$key/$translations{$key}/ig;
+}
+
+# print result
+print "Translation: $line\n";
